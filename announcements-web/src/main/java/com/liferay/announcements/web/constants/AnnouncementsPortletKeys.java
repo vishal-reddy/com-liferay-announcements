73df512a539f0171262ba8f780b44f8f4c343175
Vishal Reddy <vishal.reddy@liferay.com>
2016-05-04 11:16:39 -0700

    valid commit but missing gradle files

--- announcements-web/src/main/java/com/liferay/announcements/web/constants/AnnouncementsPortletKeys.java
+++ announcements-web/src/main/java/com/liferay/announcements/web/constants/AnnouncementsPortletKeys.java
@@ -17,6 +17,7 @@ package com.liferay.announcements.web.constants;
 /**
  * @author Adolfo Pérez
  */
+//totally valid change but missing gradle files
 public class AnnouncementsPortletKeys {
 
 	public static final String ALERTS =
